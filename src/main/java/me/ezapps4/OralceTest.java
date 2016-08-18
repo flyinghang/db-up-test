942aceb7e5622fe8e2b9c7c1c58831c5eec94d7d
flyinghang <flyinghang@gmail.com>
2016-08-19 00:36:17 +1000

    add Tester and OralceTest

---
+++ src/main/java/me/ezapps4/OralceTest.java
@@ -0,0 +1,16 @@
+package main.java.me.ezapps4;
+
+/**
+ * Created by sheldon on 19/08/2016.
+ */
+public class OralceTest implements Tester {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void test() {
+
+    }
+}
---
+++ src/main/java/me/ezapps4/Tester.java
@@ -0,0 +1,13 @@
+package main.java.me.ezapps4;
+
+/**
+ * Created by sheldon on 19/08/2016.
+ */
+public interface Tester {
+
+    void init();
+
+    void test();
+
+
+}
